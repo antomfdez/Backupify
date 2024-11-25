243cba418d6f4d6917e7f286bce460727a65b5b4
antomfdez <antonio.mesa.fdez@gmail.com>
2024-11-25 23:43:25 +0100

    Base structure

---
+++ cmd/Backupify/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/antomfdez/Backupify/internal/testmodule"
+)
+
+func main() {
+	testmodule.Hello()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/antomfdez/Backupify
+
+go 1.21.13
